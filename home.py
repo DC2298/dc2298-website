0a1ce6262a6abb9a87b804561a2a484a4c94ceca
CastixGitHub <vinci98gl@gmail.com>
2017-03-28 22:40:46 +0200

    models separed, sqlite3 used

--- home.py
+++ home.py
@@ -1,33 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from models import *
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/db'
-db = SQLAlchemy(app)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.app = app
+db.init_app(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-    def __repr__(self):
-        return '<User %r>' % self.username
 
 @app.route("/")
 def home():
-    return render_template('form.html')
+    return render_template('home.html')
 
-@app.route("/user", methods=['POST'])
+
+@app.route("/user", methods=['GET', 'POST'])
 def user():
-    user = User(request.form['username'], request.form['email'])
-    db.session.add(user)
-    db.session.commit
+    if request.method == 'POST':
+        user = User(username=request.form['username'],
+                    email=request.form['email'])
+        db.session.add(user)
+        db.session.commit()
+        
     return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
